78b735c4b3033256df59718e96111a631d613d26
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-06-15 18:47:11 -0300

    inicial

---
+++ nodes/speech_synthesizer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from butia_speech.srv import *
+from std_msgs.msg import Bool
+from gtts import gTTS, lang
+from pygame import mixer
+import rospy
+
+def synthesize_speech(req):
+    tts = gTTS(req.speech, lang=req.lang)
+    tts.save('speech.mp3')
+    mixer.init()
+    mixer.music.load('speech.mp3')
+    mixer.music.play()
+    return SynthesizeSpeechResponse(Bool(True))
+
+
+if __name__ == '__main__':
+    rospy.init_node('speech_synthesizer')
+    rospy.Service('butia/synthesize_speech', SynthesizeSpeech, synthesize_speech)
+    rospy.spin()
