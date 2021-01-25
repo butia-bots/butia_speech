e386fafa9f2b1d31b4a45b5a8466e2d65083e141
crislmfroes <cris.lima.froes@gmail.com>
2021-01-24 22:09:59 -0300

    add phrase limi timeout

--- nodes/speech_recognizer.py
+++ nodes/speech_recognizer.py
@@ -2,18 +2,33 @@
 
 import rospy
 from butia_speech.srv import SpeechToText, SpeechToTextResponse
-from butia_speech.speech_to_text import ButiaSpeechToText
-from speech_recognition import Microphone
+from speech_recognition import Microphone, Recognizer, WaitTimeoutError
+import os
+import numpy as np
+import deepspeech
+
+deepspeech_model_dir = os.path.join(os.path.dirname(__file__), '../include/deepspeech/')
+
 
 def handle_recognition(req):
-    with Microphone() as source:
-        audio = recognizer.listen(source, phrase_time_limit=8)
+    with Microphone(sample_rate=deepspeech_model.sampleRate()) as source:
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        rospy.loginfo('Listening ...')
+        try:
+            audio = recognizer.listen(source, phrase_time_limit=10, timeout=3)
+            fs = audio.sample_rate
+            audio = np.frombuffer(audio.frame_data, np.int16)
+            text = deepspeech_model.stt(audio)
+        except WaitTimeoutError:
+            text = ''
     return SpeechToTextResponse(
-        text=recognizer.recognize_deepspeech(audio)
+        text=text
     )
 
 if __name__ == '__main__':
     rospy.init_node('speech_recognizer')
-    recognizer = ButiaSpeechToText()
-    recognition_service = rospy.Service('/butia_speech/transcribe', SpeechToText, handle_recognition)
+    recognizer = Recognizer()
+    deepspeech_model = deepspeech.Model(deepspeech_model_dir + 'deepspeech-0.9.3-models.pbmm')
+    deepspeech_model.enableExternalScorer(deepspeech_model_dir + 'deepspeech-0.9.3-models.scorer')
+    recognition_service = rospy.Service('/butia_speech/asr/transcribe', SpeechToText, handle_recognition)
     rospy.spin()
