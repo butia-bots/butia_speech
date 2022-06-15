42418ef74e7d4fc49f98127db063c88a04c87e62
crislmfroes <cris.lima.froes@gmail.com>
2022-06-15 17:08:49 -0300

    Use espnet2 for audio tasks

--- nodes/speech_recognizer.py
+++ nodes/speech_recognizer.py
@@ -2,23 +2,24 @@
 
 import rospy
 from butia_speech.srv import SpeechToText, SpeechToTextResponse
-from speech_recognition import Microphone, Recognizer, WaitTimeoutError
+from speech_recognition import Microphone, Recognizer, WaitTimeoutError, AudioData
 import os
 import numpy as np
-import deepspeech
+import string
+from espnet_model_zoo.downloader import ModelDownloader
+from espnet2.bin.asr_inference import Speech2Text
 
-deepspeech_model_dir = os.path.join(os.path.dirname(__file__), '../include/deepspeech/')
-
+tag = 'Shinji Watanabe/spgispeech_asr_train_asr_conformer6_n_fft512_hop_length256_raw_en_unnorm_bpe5000_valid.acc.ave'
 
 def handle_recognition(req):
-    with Microphone(sample_rate=deepspeech_model.sampleRate()) as source:
+    with Microphone(sample_rate=16000) as source:
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
         rospy.loginfo('Listening ...')
         try:
             audio = recognizer.listen(source, phrase_time_limit=10, timeout=3)
             fs = audio.sample_rate
             audio = np.frombuffer(audio.frame_data, np.int16)
-            text = deepspeech_model.stt(audio)
+            text, *_ = speech2text(audio)[0]
         except WaitTimeoutError:
             text = ''
     return SpeechToTextResponse(
@@ -27,8 +28,20 @@ def handle_recognition(req):
 
 if __name__ == '__main__':
     rospy.init_node('speech_recognizer')
+    d = ModelDownloader()
+    speech2text = Speech2Text(
+        **d.download_and_unpack(tag),
+        device="cuda",
+        minlenratio=0.0,
+        maxlenratio=0.0,
+        ctc_weight=0.3,
+        beam_size=10,
+        batch_size=0,
+        nbest=1
+    )
     recognizer = Recognizer()
-    deepspeech_model = deepspeech.Model(deepspeech_model_dir + 'deepspeech-0.9.3-models.pbmm')
-    deepspeech_model.enableExternalScorer(deepspeech_model_dir + 'deepspeech-0.9.3-models.scorer')
+    #deepspeech_model = deepspeech.Model(deepspeech_model_dir + 'deepspeech-0.9.3-models.pbmm')
+    #deepspeech_model.enableExternalScorer(deepspeech_model_dir + 'deepspeech-0.9.3-models.scorer')
+    #stt = pipeline(task="automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
     recognition_service = rospy.Service('/butia_speech/asr/transcribe', SpeechToText, handle_recognition)
     rospy.spin()
--- nodes/speech_synthesizer.py
+++ nodes/speech_synthesizer.py
@@ -2,9 +2,17 @@
 # coding: utf-8
 from butia_speech.srv import *
 from std_msgs.msg import Bool
-from gtts import gTTS, lang
+from espnet2.bin.tts_inference import Text2Speech
+from espnet2.utils.types import str_or_none
+from scipy.io import wavfile
 from pygame import mixer
+import torch
+import time
 import rospy
+import numpy as np
+
+tag = 'kan-bayashi/ljspeech_vits'
+vocoder_tag = "none"
 
 def synthesize_speech(req):
     #tts = gTTS(req.speech, lang=req.lang)
@@ -13,12 +21,35 @@ def synthesize_speech(req):
     #mixer.music.load('speech.mp3')
     #mixer.music.play()
     #return SynthesizeSpeechResponse(Bool(True))
+    # synthesis
+    with torch.no_grad():
+        start = time.time()
+        wav = text2speech(req.speech)["wav"]
+        wavfile.write('fala.wav', text2speech.fs, (wav.view(-1).cpu().numpy()*32768).astype(np.int16))
     mixer.init()
-    mixer.music.load('fala.mpeg')
+    mixer.music.load('fala.wav')
     mixer.music.play()
     return SynthesizeSpeechResponse(Bool(True))
 
 if __name__ == '__main__':
+    text2speech = Text2Speech.from_pretrained(
+        model_tag=str_or_none(tag),
+        vocoder_tag=str_or_none(vocoder_tag),
+        device="cuda",
+        # Only for Tacotron 2 & Transformer
+        threshold=0.5,
+        # Only for Tacotron 2
+        minlenratio=0.0,
+        maxlenratio=10.0,
+        use_att_constraint=False,
+        backward_window=1,
+        forward_window=3,
+        # Only for FastSpeech & FastSpeech2 & VITS
+        speed_control_alpha=1.0,
+        # Only for VITS
+        noise_scale=0.333,
+        noise_scale_dur=0.333,
+    )
     rospy.init_node('speech_synthesizer')
     rospy.Service('butia/synthesize_speech', SynthesizeSpeech, synthesize_speech)
     rospy.spin()
