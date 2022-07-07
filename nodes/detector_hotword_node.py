beab75761d584a6fe3fc1eae4d24cc752d9f3b8f
PedroCorcaque <pedrollcorc@gmail.com>
2022-07-08 00:55:52 +0700

    changed hotword detector to new version of the library

--- nodes/detector_hotword_node.py
+++ nodes/detector_hotword_node.py
@@ -5,20 +5,25 @@ import rospkg
 
 from butia_speech.detect_hotword import DetectHotWord
 
-from std_msgs.msg import Empty, String
+from std_msgs.msg import Empty
 
 PACK_DIR = rospkg.RosPack().get_path('butia_speech')
 
 if __name__ == '__main__':
     rospy.init_node('detector_hotword_node', anonymous = True)
+    
     sensibility = rospy.get_param("/butia_hotword_detection/sensibility", 0.5)
-    library = PACK_DIR + '/include/lib/libpv_porcupine.so'
-    model = PACK_DIR + '/include/model/porcupine_params.pv'
-    keyword = PACK_DIR + '/resources/hello doris_linux.ppn'
+
+    keyword = [PACK_DIR + '/resources/Hello-Doris_en_linux_v2_1_0.ppn', PACK_DIR + '/resources/Follow-me_en_linux_v2_1_0.ppn']
+
+    sensibility = [sensibility]*len(keyword)
+
     detector_publisher_param = rospy.get_param("publishers/butia_hotword_detection/topic","/butia_speech/bhd/detected")
     detector_subscriber_param = rospy.get_param("subscribers/butia_hotword_detection/topic","/butia_speech/bhd/hot_word")
     detector_publisher = rospy.Publisher(detector_publisher_param, Empty, queue_size=1)
-    detector = DetectHotWord(library, model, keyword, sensibility)
+
+    detector = DetectHotWord(keyword, sensibility)
+
     detector.hear()
     while not rospy.is_shutdown():
         result = detector.process()
