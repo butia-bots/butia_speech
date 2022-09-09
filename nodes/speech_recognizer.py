e4a0b7fb648eaa435a4a7ced004cb0766ba7f11b
Igor Maurell <igormaurell@gmail.com>
2022-09-09 10:57:51 -0300

    sr: adding adapt to env noise

--- nodes/speech_recognizer.py
+++ nodes/speech_recognizer.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python3
 
 import rospy
+import rospkg
+from std_srvs.srv import Empty, EmptyResponse
 from butia_speech.srv import SpeechToText, SpeechToTextResponse
 from speech_recognition import Microphone, Recognizer, WaitTimeoutError, AudioData
 import os
 import numpy as np
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, pipeline
 import torch
+
+AUDIO_DIR = os.path.join(rospkg.RosPack().get_path("butia_speech"), "audios/")
+FILENAME = str(AUDIO_DIR) + "speech_input.wav"
+
+def handle_adjust_noise(req):
+    with Microphone(sample_rate=16000) as source:
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+    return EmptyResponse()
 
 def handle_recognition(req):
     with Microphone(sample_rate=16000) as source:
         #recognizer.adjust_for_ambient_noise(source, duration=0.5)
         try:
-            #audio = recognizer.listen(source, phrase_time_limit=10, timeout=3)
-            audio = recognizer.listen(source)
+            audio = recognizer.listen(source, phrase_time_limit=20, timeout=3)
+            # audio = recognizer.listen(source)
             
-            fs = audio.sample_rate
-            audio = np.frombuffer(audio.frame_data, np.int16)
+            #fs = audio.sample_rate
+            #audio = np.frombuffer(audio.frame_data, np.double)
+            #print(audio.shape)
             # tokenize
-            input_values = processor(audio.reshape((1,-1)), return_tensors="pt", padding="longest").input_values  # Batch size 1
+            #input_values = processor(audio, return_tensors="pt", sampling_rate=16000)  # Batch size 1
             
             # retrieve logits
-            logits = model(input_values[0].double()).logits
+            #with torch.no_grad():
+            #    logits = model(**input_values).logits
             
             # take argmax and decode
-            predicted_ids = torch.argmax(logits, dim=-1)
-            transcription = processor.batch_decode(predicted_ids)
-            text = transcription[0]
+            #predicted_ids = torch.argmax(logits, dim=-1)
+            #transcription = processor.batch_decode(predicted_ids)
+            #text = transcription[0]
+            with open(FILENAME, 'wb') as f:
+                f.write(audio.get_wav_data())
+            text = asr_pipeline(FILENAME)['text'].lower()
         except WaitTimeoutError:
             text = ''
     return SpeechToTextResponse(
@@ -34,12 +49,15 @@ def handle_recognition(req):
     )
 
 if __name__ == '__main__':
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h-lv60")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60")
+    #processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h-lv60")
+    #model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60")
+    asr_pipeline = pipeline(task="automatic-speech-recognition", model="facebook/wav2vec2-large-960h-lv60")
     recognizer = Recognizer()
     rospy.init_node('speech_recognizer')
     
     recognizer_service_param = rospy.get_param("/services/speech_recognizer/service", "/butia_speech/sr/speech_recognizer")
+    adjust_noise_param = rospy.get_param("/services/adjust_noise/service", "/butia_speech/sr/adjust_noise")
 
     recognition_service = rospy.Service(recognizer_service_param, SpeechToText, handle_recognition)
+    adjust_noise_service = rospy.Service(adjust_noise_param, Empty, handle_adjust_noise)
     rospy.spin()
