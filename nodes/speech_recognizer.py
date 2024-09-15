1a7351ee85aee0e250d0d65ec23469fc3cab7a85
luisffee <luizffeelix@gmail.com>
2024-09-15 15:11:09 -0300

    :wrench: fix: Fix config file

--- nodes/speech_recognizer.py
+++ nodes/speech_recognizer.py
@@ -27,32 +27,22 @@ TALK_AUDIO = os.path.join(AUDIO_DIR, "beep.wav")
 DEFAULT_LANGUAGE = 'en'
 
 def handle_recognition(req):
+    configs = rospt.getparam("/stt_configs/")
     rospy.loginfo("Initializing RealtimeSTT test... \n")
 
     if req.prompt != '':
         rospy.loginfo(f'Prompt to make easier the recognition: {req.prompt}')
 
-    recorder_config = {
-        'compute_type': 'float32',
-        'spinner': False,
-        'model': 'base.en',
+    config.update({
         'language': req.language if req.language != '' else DEFAULT_LANGUAGE,
-        'silero_sensitivity': 0.6,
-        'device': "cuda",
-        'webrtc_sensitivity': 1,
-        'post_speech_silence_duration': 0.2,
-        'min_length_of_recording': 0.5,
-        'min_gap_between_recordings': 0,
-        'enable_realtime_transcription': False,
-        'silero_deactivity_detection': True,
-        'on_recording_start': print("Starting Record..."),
+        'on_recording_start': lambda: rospy.loginfo("Starting Record..."),
         'on_vad_detect_start': lambda: playsound(TALK_AUDIO),
-        'on_vad_detect_stop': lambda: print("Finished Listening..."),
-        'on_recording_stop': lambda: print("Processing..."),
-    }
+        'on_vad_detect_stop': lambda: rospy.loginfo("Finished Listening..."),
+        'on_recording_stop': lambda: rospy.loginfo("Processing...")
+        })
 
     try:
-        with AudioToTextRecorder(**recorder_config) as recorder:
+        with AudioToTextRecorder(**configs) as recorder:
             text = recorder.text()
         try:
             AudioToTextRecorder.shutdown()
@@ -64,15 +54,9 @@ def handle_recognition(req):
     return text
 
 if __name__ == '__main__':
-    recognizer = Recognizer()
     rospy.init_node('speech_recognizer')
     
     recognizer_service_param = rospy.get_param("~services/speech_recognizer/service", "/butia_speech/sr/speech_recognizer")
-    noise_adjust_duration = rospy.get_param("~noise_adjust_duration", 1)
-    whisper_model = rospy.get_param("~whisper_model", "small")
-    phrase_time_limit = rospy.get_param("~phrase_time_limit", 3)
-    timeout = rospy.get_param("~timeout", 3)
-    sample_rate = rospy.get_param("~sample_rate", 16000)
 
     recognition_service = rospy.Service(recognizer_service_param, SpeechToText, handle_recognition)
 
