c4b94438a078efe6b1800f565541624e1f8668ec
FernandoPM28 <ferpivamendes@gmail.com>
2024-10-01 13:35:03 -0300

    :sparkles: feat: Riva tests

--- nodes/test_riva.py
+++ nodes/test_riva.py
@@ -15,11 +15,12 @@ req = {
         "language_code"  : "en-US",
         "encoding"       : riva.client.AudioEncoding.LINEAR_PCM ,   # LINEAR_PCM and OGGOPUS encodings are supported
         "sample_rate_hz" : sample_rate_hz,                          # Generate 44.1KHz audio
-        "voice_name"     : "English-US.Female-1"                    # The name of the voice to generate
+        "voice_name"     : "English-US.Female-1",                 # The name of the voice to generate
 }
+print(req.items())
 
-req["text"] = "Is it recognize speech or wreck a nice beach?"
-resp = riva_tts.synthesize(**req)
+text = "Hello, i am called Boris, a helper open source robot designed to help at domestic chores. I am a product of development of the team FBOT from FURG university. It is a pleasure to meet you!"
+resp = riva_tts.synthesize(custom_dictionary=req, text=text)
 audio_samples = np.frombuffer(resp.audio, dtype=np.int16)
 
 sf.write('test_riva_audio.wav', audio_samples, sample_rate_hz)
