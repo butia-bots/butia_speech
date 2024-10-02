e6753e8bb5f4d107fc6dd7e1619718838889097e
luisffee <luizffeelix@gmail.com>
2024-10-02 13:26:34 -0300

    :broom: clean: Clean unnecessary imports

--- nodes/speech_recognizer.py
+++ nodes/speech_recognizer.py
@@ -4,10 +4,8 @@
 import rospy
 import rospkg
 from butia_speech.srv import SpeechToText, SpeechToTextResponse
-#from speech_recognition import Microphone, Recognizer, WaitTimeoutError, RequestError, UnknownValueError
 import os
 import numpy as np
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, pipeline
 from playsound import playsound
 
 from RealtimeSTT import AudioToTextRecorder
