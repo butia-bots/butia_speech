b6f2df162ad407cde7db30ef7860217eab462d80
FernandoPM28 <ferpivamendes@gmail.com>
2024-10-09 19:58:17 -0300

    acces_key change

--- src/butia_speech/detect_hotword.py
+++ src/butia_speech/detect_hotword.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../include/binding/'
 # from porcupine import Porcupine
 import pvporcupine
 
-access_key="CDCzWBGwcp4g7wUrSQj1H1IWJMwh8UhV+zw7LiSCB46L8+0piJV5tw==" 
+access_key="g5pONkkK5ZY13pObfVoulaqSDdMpyyq+kuZlNpWP7lEQI9CuKMU5WA==" 
 
 class DetectHotWord():
     def __init__(self, keyword_path, sensitivity, library_path=None, model_path=None):
