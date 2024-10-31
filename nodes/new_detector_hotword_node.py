a3cf0089a0a56cdafced2802b7450fd3ac6c0b1d
RICHARD <richardjcassis@gmail.com>
2024-10-31 14:08:52 -0300

    new hotword node

---
+++ nodes/new_detector_hotword_node.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import rospy
+import os
+import rospkg
+
+from butia_speech.new_detect_hotword import newDetectHotWord
+
+from std_msgs.msg import Int16
+
+from termcolor import colored
+import warnings
+warning = rospy.get_param("warnings", False)
+if not warning:
+    warnings.filterwarnings("ignore")
+
+PACK_DIR = rospkg.RosPack().get_path('butia_speech')
+
+if __name__ == '__main__':
+    rospy.init_node('detector_hotword_node', anonymous = True)
+    
+    sensibility = rospy.get_param("/butia_hotword_detection/sensibility", 0.5)
+
+    keyword = [
+        PACK_DIR + '/resources/Hello-Boris_en_linux_v3_0_0.ppn',
+        PACK_DIR + '/resources/Follow-me_en_linux_v3_0_0.ppn',
+        PACK_DIR + '/resources/It--s-right_en_linux_v3_0_0.ppn',
+        PACK_DIR + '/resources/It--s-wrong_en_linux_v3_0_0.ppn'
+    ] 
+
+    sensibility = [sensibility]*len(keyword)
+
+    detector_publisher_param = rospy.get_param("publishers/butia_hotword_detection/topic","/butia_speech/bhd/detected")
+    detector_subscriber_param = rospy.get_param("subscribers/butia_hotword_detection/topic","/butia_speech/bhd/hot_word")
+    detector_publisher = rospy.Publisher(detector_publisher_param, Int16, queue_size=1)
+
+    detector = newDetectHotWord(keyword, sensibility)
+
+    detector.hear()
+    while not rospy.is_shutdown():
+        result = detector.process()
+        if result>=0:
+            detector_publisher.publish(result)
---
+++ src/butia_speech/new_detect_hotword.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import struct
+import numpy as np
+import pyaudio
+import rospkg
+
+#PACK_DIR = rospkg.RosPack().get_path('butia_hotword_detection')
+#sys.path.append(os.path.join(PACK_DIR, '/include/binding'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../include/binding/'))
+# from porcupine import Porcupine
+import pvporcupine
+
+access_key="Tbyk0dhsux2oYz/+GO8IGk05dCGmhTVze760CdDlA/vfLjkuGCqdRQ==" 
+class DetectHotWord():
+    def __init__(self, keyword_path, sensitivity, library_path=None, model_path=None):
+        # if library_path is None:
+        #     library_path = pvporcupine.LIBRARY_PATH
+        # if model_path is None:
+        #     model_path = pvporcupine.MODEL_PATH
+        # if library_path is None:
+        #     library_path = pvporcupine.LIBRARY_PATH
+        # if model_path is None:
+        #     model_path = pvporcupine.MODEL_PATH
+
+        self.handle = pvporcupine.create(access_key=access_key, keyword_paths=keyword_path, sensitivities=sensitivity)
+        self.mic = None
+
+    def hear(self):
+        self.pa = pyaudio.PyAudio()
+        audio_stream = self.pa.open(
+            rate=self.handle.sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer=self.handle.frame_length)
+        self.mic = audio_stream
+
+    def process(self):
+        if self.mic is not None:
+            pcm = self.mic.read(self.handle.frame_length)
+            pcm = struct.unpack_from("h" * self.handle.frame_length, pcm)
+            recorded_frames = []
+            recorded_frames.append(pcm)
+            result = self.handle.process(pcm)
+            return result #Return an integer representing the index of the hotword detected, from zero.
+        return -1
+
+    def __del__(self):
+        self.mic.close()
+        self.pa.terminate()
+        self.handle.delete()
+        # In future, save audio file
+
+    
