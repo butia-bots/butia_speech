99b2ca8c8a6a73c4a2243e6ff35cf73954d02daa
vitoranello <vitoranello@gmail.com>
2024-11-07 10:10:38 -0300

    :bug: fix: fix hotword for yes or no

--- nodes/new_detector_hotword_node.py
+++ nodes/new_detector_hotword_node.py
@@ -5,7 +5,7 @@ import rospkg
 
 from butia_speech.new_detect_hotword import newDetectHotWord
 
-from std_msgs.msg import Int16
+from std_msgs.msg import String
 
 from termcolor import colored
 import warnings
@@ -17,8 +17,15 @@ PACK_DIR = rospkg.RosPack().get_path('butia_speech')
 
 if __name__ == '__main__':
     rospy.init_node('detector_hotword_node', anonymous = True)
+    count = 1
     
     sensibility = rospy.get_param("/butia_hotword_detection/sensibility", 0.5)
+
+    result_arr = [
+        'Hello Boris',
+        'Yes Boris'
+        'No Boris',
+        ]
 
     keyword = [
         PACK_DIR + '/resources/Hello-Boris_en_linux_v3_0_0.ppn',
@@ -30,7 +37,7 @@ if __name__ == '__main__':
 
     detector_publisher_param = rospy.get_param("publishers/butia_hotword_detection/topic","/butia_speech/bhd/detected")
     detector_subscriber_param = rospy.get_param("subscribers/butia_hotword_detection/topic","/butia_speech/bhd/hot_word")
-    detector_publisher = rospy.Publisher(detector_publisher_param, Int16, queue_size=1)
+    detector_publisher = rospy.Publisher(detector_publisher_param, String, queue_size=1)
 
     detector = newDetectHotWord(keyword, sensibility)
 
@@ -38,4 +45,4 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         result = detector.process()
         if result>=0:
-            detector_publisher.publish(result)
+            detector_publisher.publish(result_arr[result])
